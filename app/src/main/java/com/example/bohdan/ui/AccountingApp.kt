ab6fd4f335d192cda20ae9fb5a39bfff4950ae0f
Rodion <pranikovrodion@gmail.com>
2025-11-30 15:20:17 +0200

    LR6

--- app/src/main/java/com/example/bohdan/ui/AccountingApp.kt
+++ app/src/main/java/com/example/bohdan/ui/AccountingApp.kt
@@ -15,6 +15,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -25,7 +29,17 @@ import com.example.bohdan.R
 
 @Composable
 fun AccountingApp(viewModel: MainViewModel) {
+    var permissionsGranted by remember { mutableStateOf(false) }
 
+    if (!permissionsGranted) {
+        RequestPermissions(
+            onPermissionsGranted = {
+                permissionsGranted = true
+            }
+        )
+    } else {
+        Text("Всі дозволи отримані")
+    }
     Scaffold { padding ->
         Column(
             modifier = Modifier
---
+++ app/src/main/java/com/example/bohdan/ui/PermissionsHandler.kt
@@ -0,0 +1,50 @@
+package com.example.bohdan.ui
+
+import android.Manifest
+import android.os.Build
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun RequestPermissions(
+    onPermissionsGranted: () -> Unit
+) {
+    val context = LocalContext.current
+
+    val permissionsToRequest = remember {
+        mutableListOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ).apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                // Android 13+
+                add(Manifest.permission.READ_MEDIA_IMAGES)
+                add(Manifest.permission.READ_MEDIA_VIDEO)
+            } else {
+
+                add(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+            }
+        }.toTypedArray()
+    }
+
+    val multiplePermissionResultLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { perms ->
+            val allGranted = perms.values.all { it }
+            if (allGranted) {
+                onPermissionsGranted()
+            } else {
+                Toast.makeText(context, "Немає всіх дозволив", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
+
+    LaunchedEffect(Unit) {
+        multiplePermissionResultLauncher.launch(permissionsToRequest)
+    }
+}
